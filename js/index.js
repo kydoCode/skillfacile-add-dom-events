7fd2b8990d6599d57db45dc247af5deb8f78ba73
Kody <kydokody@gmail.com>
2024-03-11 09:24:48 +0100

    Initiate project files:
    Skillfacile website
    Assignment guidelines: implement DOM interactions

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+// Dark mode : Créer le code pour ajouter un dark mode au site SkillFacile
+
+// Menu responsive : vous devez créer un menu responsive qui s'ouvre et se ferme, tel que représenté sur la vidéo de présentation. Petit bonus : faire la fermeture au scroll et la fermeture lorsqu'une modale s'ouvre
+
+// modale inscription newsletter : Pour la modale de newsletter, vous devez utiliser les creatElement pour créer tous les éléments qui la compose. Bonus : Mettre en place un message lorsque l'utilisateur s'inscrit.
+
+
+// Modale contact : Créer la modale de contact en utilisant la balise dialogue. Pour cette partie là, vous ne devez pas utiliser de creatElement. Bonus : Mettre en place un message lorsque l'utilisateur
+
+// Bonus supplémentaire : Fermer le menu responsive a l'ouverture de la modale
